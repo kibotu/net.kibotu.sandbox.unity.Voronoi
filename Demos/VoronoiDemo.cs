4c5c2700661c9fb70353a9b8269e40fed9f4b4c3
Jan Rabe <jan.rabe@wooga.net>
2014-09-25 22:36:38 +0200

    init

---
+++ Demos/VoronoiDemo.cs
@@ -0,0 +1,262 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Source;
+using UnityEngine;
+using UnityEngine.UI;
+using Voronoi;
+using Point = Voronoi.Point;
+
+namespace Assets.Voronoi.Demos
+{
+    public class VoronoiDemo : MonoBehaviour
+    {
+        public int numSites = 36;
+        public bool isAnimated = false;
+        public Bounds bounds;
+
+        private List<Point> sites;
+        private FortuneVoronoi voronoi;
+        private VoronoiGraph graph;
+        public InputField AmountOfSites;
+
+        public void Animate(bool isAnimated)
+        {
+            this.isAnimated = isAnimated;
+        }
+
+        void Start()
+        {
+            AmountOfSites.onSubmit.AddListener(value => numSites = int.Parse(value));
+            AmountOfSites.text.text = "" + numSites;
+
+            sites = new List<Point>();
+            voronoi = new FortuneVoronoi();
+
+            CreateSites(true, true, numSites);
+
+            CreateLines();
+        }
+
+        void FixedUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.G))
+            {
+                Reset();
+            }
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                Relax();
+            }
+            if (isAnimated)
+            {
+                Relax();
+                RedrawAsync();
+            }
+        }
+
+        public void Reset()
+        {
+            CreateSites(true, false); 
+            RedrawAsync();
+        }
+
+        public void Relax()
+        {
+            RelaxSites(1);
+            RedrawAsync();
+        }
+
+        public void RedrawAsync()
+        {
+            StopCoroutine("RedrawLines");
+            StartCoroutine("RedrawLines");
+        }
+
+        public IEnumerator RedrawLines()
+        {
+            yield return new WaitForEndOfFrame();
+            lines.Reset();
+            CreateLines();
+            yield return null;
+        }
+
+        void Compute(List<Point> sites)
+        {
+            this.sites = sites;
+            this.graph = this.voronoi.Compute(sites, this.bounds);
+        }
+
+        void CreateSites(bool clear = true, bool relax = false, int relaxCount = 2)
+        {
+            List<Point> sites = new List<Point>();
+            if (!clear)
+            {
+                sites = this.sites.Take(this.sites.Count).ToList();
+            }
+
+            // create vertices
+            for (int i = 0; i < numSites; i++)
+            {
+                Point site = new Point(Random.Range(bounds.min.x, bounds.max.x), Random.Range(bounds.min.z, bounds.max.z), 0);
+                sites.Add(site);
+            }
+
+            Compute(sites);
+
+            if (relax)
+            {
+                RelaxSites(relaxCount);
+            }
+        }
+
+        void RelaxSites(int iterations)
+        {
+            for (int i = 0; i < iterations; i++)
+            {
+                if (!this.graph)
+                {
+                    return;
+                }
+
+                Point site;
+                List<Point> sites = new List<Point>();
+                float dist = 0;
+
+                float p = 1f / graph.cells.Count * 0.1f;
+
+                for (int iCell = graph.cells.Count - 1; iCell >= 0; iCell--)
+                {
+                    global::Voronoi.Cell cell = graph.cells[iCell];
+                    float rn = Random.value;
+
+                    // probability of apoptosis
+                    if (rn < p)
+                    {
+                        continue;
+                    }
+
+                    site = CellCentroid(cell);
+                    dist = Distance(site, cell.site);
+
+                    // don't relax too fast
+                    if (dist > 2)
+                    {
+                        site.x = (site.x + cell.site.x) / 2;
+                        site.y = (site.y + cell.site.y) / 2;
+                    }
+                    // probability of mytosis
+                    if (rn > (1 - p))
+                    {
+                        dist /= 2;
+                        sites.Add(new Point(site.x + (site.x - cell.site.x) / dist, site.y + (site.y - cell.site.y) / dist));
+                    }
+                    sites.Add(site);
+                }
+
+                Compute(sites);
+            }
+        }
+
+        float Distance(Point a, Point b)
+        {
+            float dx = a.x - b.x;
+            float dy = a.y - b.y;
+            return Mathf.Sqrt(dx * dx + dy * dy);
+        }
+
+        Point CellCentroid(global::Voronoi.Cell cell)
+        {
+            float x = 0f;
+            float y = 0f;
+            Point p1, p2;
+            float v;
+
+            for (int iHalfEdge = cell.halfEdges.Count - 1; iHalfEdge >= 0; iHalfEdge--)
+            {
+                HalfEdge halfEdge = cell.halfEdges[iHalfEdge];
+                p1 = halfEdge.GetStartPoint();
+                p2 = halfEdge.GetEndPoint();
+                v = p1.x * p2.y - p2.x * p1.y;
+                x += (p1.x + p2.x) * v;
+                y += (p1.y + p2.y) * v;
+            }
+            v = CellArea(cell) * 6;
+            return new Point(x / v, y / v);
+        }
+
+        float CellArea(global::Voronoi.Cell cell)
+        {
+            float area = 0.0f;
+            Point p1, p2;
+
+            for (int iHalfEdge = cell.halfEdges.Count - 1; iHalfEdge >= 0; iHalfEdge--)
+            {
+                HalfEdge halfEdge = cell.halfEdges[iHalfEdge];
+                p1 = halfEdge.GetStartPoint();
+                p2 = halfEdge.GetEndPoint();
+                area += p1.x * p2.y;
+                area -= p1.y * p2.x;
+            }
+            area /= 2;
+            return area;
+        }
+
+        void OnDrawGizmos()
+        {
+            if (graph)
+            {
+                foreach (global::Voronoi.Cell cell in graph.cells)
+                {
+                    Gizmos.color = Color.black;
+                    Gizmos.DrawCube(new Vector3(cell.site.x, 0, cell.site.y), Vector3.one);
+
+                    if (cell.halfEdges.Count > 0)
+                    {
+                        foreach (HalfEdge halfEdge in cell.halfEdges)
+                        {
+                            Edge edge = halfEdge.edge;
+
+                            if (edge.va && edge.vb)
+                            {
+                                Gizmos.color = Color.red;
+                                Gizmos.DrawLine(new Vector3(edge.va.x, 0, edge.va.y),
+                                    new Vector3(edge.vb.x, 0, edge.vb.y));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private LinesRenderer lines;
+
+        void CreateLines()
+        {
+            if (lines == null)
+                lines = GetComponent<LinesRenderer>();
+
+            if (graph)
+            {
+                foreach (global::Voronoi.Cell cell in graph.cells)
+                {
+//                Gizmos.color = Color.black;
+//                Gizmos.DrawCube(new Vector3(cell.site.x, 0, cell.site.y), Vector3.one);
+
+                    if (cell.halfEdges.Count > 0)
+                    {
+                        foreach (HalfEdge halfEdge in cell.halfEdges)
+                        {
+                            Edge edge = halfEdge.edge;
+
+                            if (edge.va && edge.vb)
+                            {
+                                lines.AddLine(new Vector2(edge.va.x, edge.va.y), new Vector2(edge.vb.x,edge.vb.y));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ RBTree/RBTree.cs
@@ -0,0 +1,382 @@
+﻿// ---------------------------------------------------------------------------
+// Red-Black tree code (based on C version of "rbtree" by Franck Bui-Huu
+// https://github.com/fbuihuu/libtree/blob/master/rb.c
+
+namespace Voronoi
+{
+    public class RBNode
+    {
+    }
+
+    public class RBNodeBase : RBNode
+    {
+        public RBNodeBase Parent;
+        public RBNodeBase Prev;
+        public RBNodeBase Next;
+        public RBNodeBase Left;
+        public RBNodeBase Right;
+        public bool Red;
+        public bool Black { get { return !Red; } }
+
+        public RBNodeBase()
+        {
+            this.Parent = null;
+            this.Prev = null;
+            this.Next = null;
+            this.Left = null;
+            this.Right = null;
+            this.Red = false;
+        }
+
+        public static implicit operator bool(RBNodeBase a)
+        {
+            return a != null;
+        }
+    }
+
+    public class RBTree :  RBNodeBase
+    {
+        public RBNodeBase Root;
+
+        public RBTree()
+        {
+            this.Root = null;
+        }
+
+        public void Insert(RBNodeBase node, RBNodeBase successor)
+        {
+            RBNodeBase parent = null;
+            if (node)
+            {
+                // >>> rhill 2011-05-27: Performance: cache previous/next nodes
+                successor.Prev = node;
+                successor.Next = node.Next;
+                if (node.Next)
+                {
+                    node.Next.Prev = successor;
+                }
+                node.Next = successor;
+                // <<<
+                if (node.Right)
+                {
+                    // in-place expansion of node.rbRight.getFirst();
+                    node = node.Right;
+                    while (node.Left)
+                        node = node.Left;
+                    node.Left = successor;
+                }
+                else
+                {
+                    node.Right = successor;
+                }
+                parent = node;
+            }
+            // rhill 2011-06-07: if node is null, successor must be inserted
+            // to the left-most part of the tree
+            else if (Root)
+            {
+                node = GetFirst(Root);
+                // >>> Performance: cache previous/next nodes
+                successor.Prev = null;
+                successor.Next = node;
+                node.Prev = successor;
+                // <<<
+                node.Left = successor;
+                parent = node;
+            }
+            else
+            {
+                // >>> Performance: cache previous/next nodes
+                successor.Prev = null;
+                successor.Next = null;
+                // <<<
+                Root = successor;
+                parent = null;
+            }
+
+            successor.Left = null;
+            successor.Right = null;
+            successor.Parent = parent;
+            successor.Red = true;
+
+            // Fixup the modified tree by recoloring nodes and performing
+            // rotations (2 at most) hence the red-black tree properties are
+            // preserved.
+            RBNodeBase grandpa;
+            RBNodeBase uncle;
+            node = successor;
+            while (parent != null && parent.Red)
+            {
+                grandpa = parent.Parent;
+                if (parent == grandpa.Left)
+                {
+                    uncle = grandpa.Right;
+                    if (uncle != null && uncle.Red)
+                    {
+                        parent.Red = false;
+                        uncle.Red = false;
+                        grandpa.Red = true;
+                        node = grandpa;
+                    }
+                    else
+                    {
+                        if (node == parent.Right)
+                        {
+                            RotateLeft(parent);
+                            node = parent;
+                            parent = node.Parent;
+                        }
+                        parent.Red = false;
+                        grandpa.Red = true;
+                        RotateRight(grandpa);
+                    }
+                }
+                else
+                {
+                    uncle = grandpa.Left;
+                    if (uncle != null && uncle.Red)
+                    {
+                        parent.Red = false;
+                        uncle.Red = false;
+                        grandpa.Red = true;
+                        node = grandpa;
+                    }
+                    else
+                    {
+                        if (node == parent.Left)
+                        {
+                            RotateRight(parent);
+                            node = parent;
+                            parent = node.Parent;
+                        }
+                        parent.Red = false;
+                        grandpa.Red = true;
+                        RotateLeft(grandpa);
+                    }
+                }
+                parent = node.Parent;
+            }
+            Root.Red = false;
+        }
+
+        public void Remove(RBNodeBase node)
+        {
+            // >>> rhill 2011-05-27: Performance: cache previous/next nodes
+            if (node.Next)
+            {
+                node.Next.Prev = node.Prev;
+            }
+            if (node.Prev)
+            {
+                node.Prev.Next = node.Next;
+            }
+            node.Next = null;
+            node.Prev = null;
+            // <<<
+
+            RBNodeBase parent = node.Parent;
+            RBNodeBase left = node.Left;
+            RBNodeBase right = node.Right;
+            RBNodeBase next = (left == null) ? right : (right == null) ? left : GetFirst(right);
+
+            if (parent)
+            {
+                if (parent.Left == node)
+                    parent.Left = next;
+                else
+                    parent.Right = next;
+            }
+            else
+            {
+                Root = next;
+            }
+
+            //	rhill - enforce red-black rules
+            bool isRed;
+            if (left && right)
+            {
+                isRed = next.Red;
+                next.Red = node.Red;
+                next.Left = left;
+                left.Parent = next;
+                if (next != right)
+                {
+                    parent = next.Parent;
+                    next.Parent = node.Parent;
+                    node = next.Right;
+                    parent.Left = node;
+                    next.Right = right;
+                    right.Parent = next;
+                }
+                else
+                {
+                    next.Parent = parent;
+                    parent = next;
+                    node = next.Right;
+                }
+            }
+            else
+            {
+                isRed = node.Red;
+                node = next;
+            }
+            // 'node' is now the sole successor's child and 'parent' its
+            // new parent (since the successor can have been moved)
+            if (node)
+            {
+                node.Parent = parent;
+            }
+            // the 'easy' cases
+            if (isRed)
+            {
+                return;
+            }
+            if (node != null && node.Red)
+            {
+                node.Red = false;
+                return;
+            }
+            // the other cases
+            RBNodeBase sibling;
+            do
+            {
+                if (node == Root)
+                    break;
+                if (node == parent.Left)
+                {
+                    sibling = parent.Right;
+                    if (sibling.Red)
+                    {
+                        sibling.Red = false;
+                        parent.Red = true;
+                        RotateLeft(parent);
+                        sibling = parent.Right;
+                    }
+                    if ((sibling.Left != null && sibling.Left.Red) ||
+                        (sibling.Right != null && sibling.Right.Red))
+                    {
+                        if (sibling.Right == null || !sibling.Right.Red)
+                        {
+                            sibling.Left.Red = false;
+                            sibling.Red = true;
+                            RotateRight(sibling);
+                            sibling = parent.Right;
+                        }
+                        sibling.Red = parent.Red;
+                        parent.Red = false;
+                        sibling.Right.Red = false;
+                        RotateLeft(parent);
+                        node = Root;
+                        break;
+                    }
+                }
+                else
+                {
+                    sibling = parent.Left;
+                    if (sibling.Red)
+                    {
+                        sibling.Red = false;
+                        parent.Red = true;
+                        RotateRight(parent);
+                        sibling = parent.Left;
+                    }
+                    if ((sibling.Left != null && sibling.Left.Red) ||
+                        (sibling.Right != null && sibling.Right.Red))
+                    {
+                        if (sibling.Left == null || !sibling.Left.Red)
+                        {
+                            sibling.Right.Red = false;
+                            sibling.Red = true;
+                            RotateLeft(sibling);
+                            sibling = parent.Left;
+                        }
+                        sibling.Red = parent.Red;
+                        parent.Red = false;
+                        sibling.Left.Red = false;
+                        RotateRight(parent);
+                        node = Root;
+                        break;
+                    }
+                }
+                sibling.Red = true;
+                node = parent;
+                parent = parent.Parent;
+            }
+            while (!node.Red);
+
+            if (node)
+                node.Red = false;
+        }
+
+        private void RotateLeft(RBNodeBase node)
+        {
+            RBNodeBase p = node;
+            RBNodeBase q = node.Right;
+            RBNodeBase parent = p.Parent;
+            if (parent)
+            {
+                if (parent.Left == p)
+                    parent.Left = q;
+                else
+                    parent.Right = q;
+            }
+            else
+            {
+                Root = q;
+            }
+            q.Parent = parent;
+            p.Parent = q;
+            p.Right = q.Left;
+            if (p.Right)
+            {
+                p.Right.Parent = p;
+            }
+            q.Left = p;
+        }
+
+        private void RotateRight(RBNodeBase node)
+        {
+            RBNodeBase p = node;
+            RBNodeBase q = node.Left;
+            RBNodeBase parent = p.Parent;
+            if (parent)
+            {
+                if (parent.Left == p)
+                    parent.Left = q;
+                else
+                    parent.Right = q;
+            }
+            else
+            {
+                Root = q;
+            }
+            q.Parent = parent;
+            p.Parent = q;
+            p.Left = q.Right;
+            if (p.Left)
+            {
+                p.Left.Parent = p;
+            }
+            q.Right = p;
+        }
+
+        public RBNodeBase GetFirst(RBNodeBase node)
+        {
+            while (node.Left)
+                node = node.Left;
+            return node;
+        }
+
+        public RBNodeBase GetLast(RBNodeBase node)
+        {
+            while (node.Right)
+                node = node.Right;
+            return node;
+        }
+
+        public static implicit operator bool(RBTree a)
+        {
+            return a != null;
+        }
+    }
+}
